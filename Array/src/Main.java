aa282cf50f7e84200cf2c342d7f654439c67be87
maurilio <maurilioayrton@hotmail.com>
2023-07-23 21:38:09 -0300

    executando arrays

---
+++ Array/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] valores = {5,6,7,8,923,5237,8,412};
+        Arrays.sort(valores);
+        System.out.println(Arrays.toString(valores));
+        }
+    }
